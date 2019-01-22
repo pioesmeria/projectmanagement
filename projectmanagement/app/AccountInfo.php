edd08e957b64655df21ea1ce74f85509cf0f2b0e
Erwin Raphael Piolo Esmeria <p.esmeria11@gmail.com>
2019-01-22 16:49:44 +0800

    models and controllers

---
+++ projectmanagement/app/AccountInfo.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AccountInfo extends Model
+{
+    public $primaryKey = 'account_info_id';
+}
---
+++ projectmanagement/app/Task.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    public $primaryKey = 'task_id';
+}
